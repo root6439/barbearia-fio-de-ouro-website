bbca262c8b9de92532be3758df00cd68e6fda0ae
root6439 <nicolas.soaresh.98@gmail.com>
2022-08-23 21:57:19 -0300

    ajuste server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-app.use(express.static(__dirname + "/dist/barbearia-fio-de-ouro-website"));
+app.use(express.static(__dirname + "/dist/fio-de-ouro"));
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/barbearia-fio-de-ouro-website/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/fio-de-ouro/index.html"));
 });
 app.listen(process.env.PORT || 8080);
